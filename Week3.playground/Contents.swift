7ccb66c0af590fbaafead9f6b09f62eca5e97b16
Jeffrey Jacka <jtjacka@gmail.com>
2015-08-19 11:39:44 -0700

    Complete Wednesday code challenge

--- Week3.playground/Contents.swift
+++ Week3.playground/Contents.swift
@@ -48,3 +48,28 @@ func returnMiddleIndex(arr : [Int]) -> [Int] {
 
 let returnArray = returnMiddleIndex(TestArray4)
 let returnArray2 = returnMiddleIndex(TestArray5)
+
+//MARK: Wednesday
+func almostMultipeOfTen(x : Int) -> Bool {
+  
+  if x % 10 <= 2 {
+    return true
+  } else if x % 10 >= 8 {
+    return true
+  } else {
+    return false
+  }
+}
+
+almostMultipeOfTen(13)
+almostMultipeOfTen(12)
+almostMultipeOfTen(9)
+almostMultipeOfTen(8)
+almostMultipeOfTen(7)
+almostMultipeOfTen(24)
+almostMultipeOfTen(22)
+almostMultipeOfTen(19)
+
+
+
+ 
